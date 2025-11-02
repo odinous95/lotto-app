ee55fc7fcd5c1cd170af498bffefef662f124148
Odin Thif <mjd.thif@icloud.com>
2025-11-02 16:22:12 +0100

    add Lotto contract as a placeholder for lottery system implementation

---
+++ src/Lotto.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**
+ * @title Lotto
+ * @author Odinous
+ * @notice A simple lottery contract
+ * @dev This contract is a placeholder for a lottery system implementation.
+ */
+contract Lotto {
+    // 1. enter the lotto
+    // pick a a winner
+    function enterLotto() public {
+        // Implementation goes here
+    }
+    function pickWinner() public {
+        // Implementation goes here
+    }
+}
