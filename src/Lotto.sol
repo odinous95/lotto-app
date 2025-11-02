3fcc3354261531026cde60983040292fc24eef87
Odin Thif <mjd.thif@icloud.com>
2025-11-02 21:32:08 +0100

    #8  set up chainlink VFR contract enhance Lotto contract by adding keyHash and subId parameters to constructor, and implementing random words request for winner selection

--- src/Lotto.sol
+++ src/Lotto.sol
@@ -18,14 +18,19 @@ contract Lotto is VRFConsumerBaseV2Plus {
 
     uint256 private immutable i_entranceFee;
     uint256 private immutable i_lotto_interval; // Time interval for picking a winner in seconds
-    address payable[] private s_players;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subId;
+    uint16 private constant REQUEST_CONFIRMETION = 3;
+    uint32 private constant CALLBACK_GAS_LIMIT = 100000; // Gas limit for the callback function
+    uint32 private constant NUM_WORDS = 1; // Number of random words to request
     uint256 private s_lastPickedTime; // Timestamp of the last winner pick (snapshot)
+    address payable[] private s_players;
 
     // Events -=-=-=-=-=-=-=-------=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     event LottoEntered(address indexed player);
 
     // Constructor -=-=-=-=-=-=-=-------=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    constructor(uint256 _entranceFee, uint256 _interval, address vrfCoordinator)
+    constructor(uint256 _entranceFee, uint256 _interval, address vrfCoordinator, bytes32 keyHash, uint64 subId)
         VRFConsumerBaseV2Plus(vrfCoordinator)
     {
         i_entranceFee = _entranceFee;
@@ -46,15 +51,26 @@ contract Lotto is VRFConsumerBaseV2Plus {
         if (block.timestamp - s_lastPickedTime < i_lotto_interval) {
             revert Lotto__NotEnoughTimePassed();
         }
-        // if passed we pick a winner (placeholder logic)
-        // we get it from chainlink VRF
+        VRFV2PlusClient.RandomWordsRequest request = VRFV2PlusClient.RandomWordsRequest({
+            keyHash: i_keyHash,
+            subId: i_subId,
+            requestConfirmations: REQUEST_CONFIRMETION,
+            callbackGasLimit: CALLBACK_GAS_LIMIT,
+            numWords: NUM_WORDS,
+            extraArgs: VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: false})) // new parameter
+        });
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
     }
 
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {
+        // Implementation for selecting a winner using randomWords
+    }
     // Getter functions -=-=-=-=-=-=-=-------=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     /**
      * @notice Returns the entrance fee for the lottery
      * @return The entrance fee in wei
      */
+
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
