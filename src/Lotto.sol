80bc5d67370bacbd96b26a77da52152f82a598ca
Odin Thif <mjd.thif@icloud.com>
2025-11-02 16:29:20 +0100

    refactor Lotto contract to include entrance fee handling and update function visibility

--- src/Lotto.sol
+++ src/Lotto.sol
@@ -8,12 +8,22 @@ pragma solidity ^0.8.19;
  * @dev This contract is a placeholder for a lottery system implementation.
  */
 contract Lotto {
-    // 1. enter the lotto
-    // pick a a winner
-    function enterLotto() public {
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 _entranceFee) {
+        i_entranceFee = _entranceFee;
+    }
+
+    function enterLotto() public payable {
         // Implementation goes here
     }
     function pickWinner() public {
         // Implementation goes here
+    }
+
+    // Getter functions
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
     }
 }
