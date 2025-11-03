bf8913a0d36accfc38002dd6d671d0f56436d67c
Odin Thif <mjd.thif@icloud.com>
2025-11-03 23:50:03 +0100

    refactor: simplify run function and prepare deployContract for Lotto and HelperConfig deployment

--- script/LottoDeploy.s.sol
+++ script/LottoDeploy.s.sol
@@ -3,15 +3,10 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {Lotto} from "../src/Lotto.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract LottoDeploy is Script {
-    function run() external {
-        uint256 entranceFee = 0.1 ether;
-        uint256 interval = 300; // 5 minutes
-        address vrfCoordinator = 0xYourVrfCoordinatorAddressHere; // Replace with actual VRF Coordinator address
+    function run() external {}
 
-        vm.startBroadcast();
-        Lotto lotto = new Lotto(entranceFee , interval,vrfCoordinator );
-        vm.stopBroadcast();
-    }
+    function deployContract() external returns (Lotto, HelperConfig) {}
 }
