9a84fead8ccbb7ab44afd7564504a1b4250570c8
Odin Thif <mjd.thif@icloud.com>
2025-11-10 10:43:07 +0100

    feat: implement deployContract function to deploy Lotto and HelperConfig contracts

--- script/LottoDeploy.s.sol
+++ script/LottoDeploy.s.sol
@@ -6,7 +6,23 @@ import {Lotto} from "../src/Lotto.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract LottoDeploy is Script {
-    function run() external {}
+    function run() public {}
 
-    function deployContract() external returns (Lotto, HelperConfig) {}
+    function deployContract() external returns (Lotto, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory networkConfig = helperConfig.getLocalConfig();
+
+        vm.startBroadcast();
+        Lotto lotto = new Lotto(
+            networkConfig.entranceFee,
+            networkConfig.lotteryInterval,
+            networkConfig.vrfCoordinator,
+            networkConfig.keyHash,
+            networkConfig.subId,
+            networkConfig.callbackGasLimit
+        );
+        vm.stopBroadcast();
+
+        return (lotto, helperConfig);
+    }
 }
